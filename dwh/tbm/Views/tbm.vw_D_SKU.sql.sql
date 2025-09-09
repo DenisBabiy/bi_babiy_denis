e8e0b56b577569347c72e6f73c6edd1f73061ab9
Denis Babiy <denis.babiy@ics-it.ru>
2025-09-09 11:38:22 +0300

    работа в рамках урока 3.Доработка Tabular-куба

--- dwh/tbm/Views/tbm.vw_D_SKU.sql.sql
+++ dwh/tbm/Views/tbm.vw_D_SKU.sql.sql
@@ -1,4 +1,6 @@
 --!extension
+--!dep Table/bi.BusinessUnit
+--!dep Table/md_bi.SKUAdditionalInfo
 create or alter view [tbm].[vw_D_SKU]
 as
 select
@@ -6,8 +8,11 @@ select
 	,isnull(s.Name, 'Unknown') collate Cyrillic_General_100_CI_AI				as SKUName
 	,isnull(s.CodeManufacturer, 'Unknown') collate Cyrillic_General_100_CI_AI	as SkuCodeManufacturer
 	,isnull(b.Name, 'Unknown') collate Cyrillic_General_100_CI_AI				as BrandName
+	,isnull(bu.Name, 'Unknown') collate Cyrillic_General_100_CI_AI				as BusinessUnit
 from dim.SKU as s
 	left join dim.Brand as b on b.ID = s.ID_Brand
+	LEFT JOIN md_bi.SKUAdditionalInfo AS sai ON sai.ID = s.ID
+	LEFT JOIN bi.BusinessUnit AS bu ON bu.ID = sai.ID_bi_BusinessUnit
 where s.FlagExclude = 0
 	and (s.ID_mapping_DataSource = 1
 		or s.ID = -1
