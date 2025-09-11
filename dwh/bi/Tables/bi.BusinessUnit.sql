fcb8cba576b7ddefee6fccb6e570ef1c75a9d4bf
Denis Babiy <denis.babiy@ics-it.ru>
2025-09-11 09:40:08 +0300

    Исправления после ревью_3

--- dwh/bi/Tables/bi.BusinessUnit.sql
+++ dwh/bi/Tables/bi.BusinessUnit.sql
@@ -3,7 +3,6 @@ create table [bi].[BusinessUnit] (
 	[Code] varchar(63) not null,
 	[Name] varchar(255) not null,
 	[MDT_DateCreate] datetime not null,
-	[MDT_ID_PrincipalCreatedBy] int not null,
 	[MDT_FlagActive] bit not null,
 	[MDT_DateDeleted] datetime null,
 	constraint [PK_dwh_BusinessUnit] primary key clustered ([ID] asc) with (STATISTICS_NORECOMPUTE = ON)
